b342cd27e1ecdaab008410be859dcd0627127694
Indoorfish1 <broland607@gmail.com>
2024-08-15 17:37:24 -0400

    removed arm, intake, outtake constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -100,61 +100,6 @@ public final class Constants {
     public static final double kTURN_GEAR_RATIO = 150.0 / 7.0;
   }
 
-  public static final class IntakeConstants {
-    public static final int kBeamBreakChannel = 0;
-    public static final double kVomitDelay = 0.5;
-    public static final double kP = 6.0;
-    public static final double kI = 0.0;
-    public static final double kEatRPM = 800.0;
-    public static final double kVomitRPM = -1000.0;
-    public static final double kPoopRPM = 1000.0;
-    public static final double kDigestRPM = 600.0;
-  }
-
-  public static final class ArmConstants {
-    // NO GO TO POSITION ANGLES SHOULD BE OVER 108 DEGREES
-    public static final double armIntakePosDeg = 0.0;
-    public static final double armDrivePosDeg = 45.0;
-    public static final double armAmpPosDeg = 108.0;
-    public static final double armTrapPosDeg = 75.0;
-    public static final double armStartPosDeg = 90.0; // pulls arm off of brake
-
-    public static final int ARM_LOCK_SOLENOID_CHANNEL = 6;
-    public static final int ARM_SOLENOID_CHANNEL = 7;
-
-    public static final int ARM_CURRENT_LIMIT = 35;
-
-    public static final double MIN_ANGLE_RADS = 0.0;
-    public static final double MAX_ANGLE_RADS = Math.PI;
-
-    public static final double ARM_MASS_LBS = 25.0;
-    public static final double ARM_COM_DISTANCE_INCHES = 20.0;
-    public static final double ARM_LENGTH_METERS = ARM_COM_DISTANCE_INCHES * MeterPerInch;
-
-    // 1:98 planetary gear ratio
-    public static final double ARM_GEAR_REDUCTION = 98.0;
-    public static final double ARM_GEAR_RATIO = 1.0 / ARM_GEAR_REDUCTION;
-
-    public static final double ARM_FF_KS = 0.0; // TODO: tune these
-    public static final double ARM_FF_KG = 0.4;
-    public static final double ARM_FF_KV = 1.91;
-    public static final double ARM_FF_KA = 0.05;
-
-    public static final double ARM_P = 4.0;
-    public static final double ARM_I = 3.0;
-    public static final double ARM_D = 0.0;
-    public static final double ARM_ACCEL_LIMIT = 1.1;
-    public static final double ARM_VEL_LIMIT = 0.85167;
-  }
-
-  public static final class OuttakeConstants {
-    public static final double kP = 1.0; // TODO: tune these
-    public static final double kD = 0.0;
-    public static final double GEAR_RATIO_REDUCTION = 3.0;
-    ;
-    public static final double CURRENT_LIMIT = 35.0;
-  }
-
   public static final class AutoConstants {
     public static final double driveFinishThreshold = 0.075; // TODO: tune these
     public static final double angleFinishThreshold = Math.PI / 12.0;
